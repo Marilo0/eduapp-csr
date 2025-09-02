192e5fc113b34818375c25a001e55170b93cd204
Marilo0 <marialenabarbopoulou@gmail.com>
2025-09-02 13:55:54 +0300

    fix: import Enable Auditing

--- src/main/java/gr/aueb/cf/eduapp/EduappApplication.java
+++ src/main/java/gr/aueb/cf/eduapp/EduappApplication.java
@@ -2,6 +2,7 @@ package gr.aueb.cf.eduapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
