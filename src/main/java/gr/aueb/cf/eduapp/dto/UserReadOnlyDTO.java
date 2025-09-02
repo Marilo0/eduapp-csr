01d40aee180da7775094aaa70e5795cd80bb054c
Marilo0 <marialenabarbopoulou@gmail.com>
2025-09-02 14:55:38 +0300

    fix: change from class(Lombok) to record

--- src/main/java/gr/aueb/cf/eduapp/dto/UserReadOnlyDTO.java
+++ src/main/java/gr/aueb/cf/eduapp/dto/UserReadOnlyDTO.java
@@ -1,16 +1,22 @@
+//package gr.aueb.cf.eduapp.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Getter
+//@Setter
+//public class UserReadOnlyDTO {
+//    private String firstname;
+//    private String lastname;
+//    private String vat;
+//}
 package gr.aueb.cf.eduapp.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Builder;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-public class UserReadOnlyDTO {
-    private String firstname;
-    private String lastname;
-    private String vat;
-}
+@Builder
+public record UserReadOnlyDTO(String firstname, String lastname, String vat) {}
