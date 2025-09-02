4f441ed9b7752a39d18dfa3ce0d064b76ad6096f
Marilo0 <marialenabarbopoulou@gmail.com>
2025-09-02 15:24:26 +0300

    fix: remove unused import

--- src/main/java/gr/aueb/cf/eduapp/repository/PersonalInfoRepository.java
+++ src/main/java/gr/aueb/cf/eduapp/repository/PersonalInfoRepository.java
@@ -12,3 +12,4 @@ public interface PersonalInfoRepository extends JpaRepository<PersonalInfo, Long
     Optional<PersonalInfo> findByAmka(String amka);
     Optional<PersonalInfo> findByIdentityNumber(String identityNumber);
 }
+
